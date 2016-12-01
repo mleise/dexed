534df12ff9e01d7bbd941f6041bcf820e3b9c3c1
Basile Burg <basile.b@gmx.com>
2016-12-01 02:55:22 +0100

    #97, non stop option not applied correctly

--- src/ce_gdb.pas
+++ src/ce_gdb.pas
@@ -1556,8 +1556,10 @@ begin
   gdbCommand('break _d_arraybounds');
   gdbCommand('break _d_switch_error');
   gdbCommand('-gdb-set mi-async on');
-  if fOptions.stopAllThreadsOnBreak then
+  if not fOptions.stopAllThreadsOnBreak then
     gdbCommand('-gdb-set non-stop on');
+  else
+    gdbCommand('-gdb-set non-stop off');
   fGdb.OnReadData := @gdboutJsonize;
   // launch
   cpuViewer.TIObject := fInspState;
