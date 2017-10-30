e1288094ab15eeee6f47ac95f114b20cd47a3ee1
Basile Burg <basile.b@gmx.com>
2017-10-30 03:21:03 +0100

    Revert "gdb commander, non stop mode was inverted"
    This reverts commit 4b16fe01283d420da5b233fd2a2052824ead3c49.

--- src/ce_gdb.pas
+++ src/ce_gdb.pas
@@ -1760,9 +1760,9 @@ begin
   gdbCommand('break _d_switch_error');
   gdbCommand('-gdb-set mi-async on');
   if not fOptions.stopAllThreadsOnBreak then
-    gdbCommand('-gdb-set non-stop off')
+    gdbCommand('-gdb-set non-stop on')
   else
-    gdbCommand('-gdb-set non-stop on');
+    gdbCommand('-gdb-set non-stop off');
   fGdb.OnReadData := @gdboutJsonize;
   cpuViewer.TIObject := fInspState;
   cpuViewer.RefreshPropertyValues;
