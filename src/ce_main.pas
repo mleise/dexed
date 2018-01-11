34d2d4e172c7ed2c0d02163ccf85481935ca2601
Basile Burg <basile.b@gmx.com>
2018-01-11 03:45:59 +0100

    remove old trick to access member

--- src/ce_main.pas
+++ src/ce_main.pas
@@ -26,11 +26,6 @@ type
 
   TCEApplicationOptions = class;
 
-  TAnchorDockSplitterEx = class(TAnchorDockSplitter)
-  public
-    property OnMouseWheel;
-  end;
-
   TAsyncWait = (awNo, awYes, awCustom);
 
   TRunnableToFolderCondition = (
@@ -1379,7 +1374,6 @@ begin
   accept := GetKeyShiftState = [ssCtrl];
 end;
 
-//TODO-cdocking: remove the usage of TAnchorDockSplitterEx from Laz 1.8 (OnMouseWheel public)
 procedure TCEMainForm.DockSplitterMw(Sender: TObject; Shift: TShiftState; WheelDelta: Integer; MousePos: TPoint; var Handled: Boolean);
 var
   offs: integer;
@@ -1415,10 +1409,10 @@ begin
         if site is TAnchorDockHostSite then
         begin
           if TAnchorDockHostSite(site).BoundSplitter.isNotNil then
-            TAnchorDockSplitterEx(TAnchorDockHostSite(site).BoundSplitter).OnMouseWheel:=@DockSplitterMw;
+            TAnchorDockHostSite(site).BoundSplitter.OnMouseWheel:= @DockSplitterMw;
         end
         else if site is TAnchorDockSplitter then
-          TAnchorDockSplitterEx(TAnchorDockSplitter(site)).OnMouseWheel:=@DockSplitterMw;
+          TAnchorDockSplitter(site).OnMouseWheel:= @DockSplitterMw;
       end;
   end;
 end;
