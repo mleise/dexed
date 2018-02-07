3788ecb57e2ba55601466e4a54473c0eefda42d5
Nafees Hassan <nafeeshassan16@gmail.com>
2018-02-08 02:36:22 +0500

    Added dependencies
    addressing Issue #215

--- cesetup/deb.sh
+++ cesetup/deb.sh
@@ -45,7 +45,7 @@ Section: devel
 Priority: optional
 Date: $dte
 Architecture: $arch
-Depends: bash
+Depends: bash, libc6, libgtk2.0-0, libgdk-pixbuf2.0-0, libglib2.0-0, libpango-1.0-0, libcairo2, libatk1.0-0, libpangocairo-1.0-0, libglib2.0-0, libfontconfig1, libxrender1, libxinerama1, libxi6, libxrandr2, libxcursor1, libxcomposite1, libxdamage1, libxfixes3, libxext6, libxcb1, libpangoft2-1.0-0, libffi6, libpcre3, libthai0, libpixman-1-0, libfreetype6, libpng16-16, libxcb-shm0, libxcb-render0, zlib1g, libselinux1, libmount1, libexpat1, libxau6, libxdmcp6, libharfbuzz0b, libdatrie1, libblkid1, libbsd0, libgraphite2-3, libuuid1
 Maintainer: Basile Burg <b2.temp@gmx.com>
 Description: IDE for the D programming language" > control
 
